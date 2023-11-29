60ece82fc873e55c2af31db04f5f142d1b23deef
Momo Kornher <kornherm@amazon.co.uk>
2023-11-29 09:33:10 +0100

    use cdk8s-plus-25

--- .projenrc.js
+++ .projenrc.js
@@ -15,7 +15,7 @@ const project = new Cdk8sTeamTypeScriptProject({
 
     // used in integration tests to author a cdk8s application.
     'cdk8s',
-    'cdk8s-plus-24',
+    'cdk8s-plus-25',
     'constructs',
     'ts-node',
 
